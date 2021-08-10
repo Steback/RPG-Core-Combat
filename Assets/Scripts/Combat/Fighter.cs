22833e3acadb393087bac5e11bd7d00fc6d8e57b
Jesus Guio <jfranciscoguio@protonmail.com>
2021-08-09 20:34:31 -0500

    Add Interact with combat and movement in player crotroller

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            print("Take that you shot, squat peasant!");   
+        }
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,3 +1,4 @@
+using Combat;
 using Movement;
 using UnityEngine;
 
@@ -7,29 +8,62 @@ namespace Control
     {
         private Mover _mover;
         private Camera _mainCamera;
+        private Fighter _fighter;
 
         // Start is called before the first frame update
         void Awake()
         {
             _mover = GetComponent<Mover>();
+            _fighter = GetComponent<Fighter>();
             _mainCamera = Camera.main;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButton(0))
-            {
-                MoveToCursor();
-            }
+            if (InteractWithCombat()) return;
+            
+            if (InteractWithMovement()) return;
         }
-    
-        private void MoveToCursor()
-        { 
-            if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out var hit)) 
-            { 
-                _mover.MoveTo(hit.point);
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMousRay());
+
+            foreach (var hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (!target) continue;
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _fighter.Attack(target);
+                }
+
+                return true;
             }
+
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+            if (Physics.Raycast(GetMousRay(), out var hit)) 
+            { 
+                if (Input.GetMouseButton(0))
+                { 
+                    _mover.MoveTo(hit.point);
+                }
+                
+                return true;
+            }
+
+            return false;
+        }
+
+        private Ray GetMousRay()
+        {
+            return _mainCamera.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
