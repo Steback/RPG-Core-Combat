3bf410fa30e1cd9c9f734a44dd3c03cdb88d110c
Jesus Guio <jfranciscoguio@protonmail.com>
2021-09-15 19:00:35 -0500

    Player spawnpoint added. Also Lighting asses added

--- Assets/Scripts/Scene/Portal.cs
+++ Assets/Scripts/Scene/Portal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace Scene
@@ -7,6 +8,7 @@ namespace Scene
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int sceneToLoad = -1;
+        [SerializeField] private Transform spawnPoint;
         
         private void OnTriggerEnter(Collider other)
         {
@@ -20,7 +22,30 @@ namespace Scene
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal portal = GetOtherPortal();
+            UpdatePlayer(portal);
+            
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(portal.spawnPoint.position);
+            player.transform.rotation = portal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
